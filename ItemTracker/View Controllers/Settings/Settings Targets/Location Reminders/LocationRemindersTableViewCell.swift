6b578d1f40837d9f329c2e6fad6094cebf85a3f5
TOPCODER10192 <webmaster86912@gmail.com>
2019-07-26 17:42:29 -0600

    July 26

---
+++ ItemTracker/View Controllers/Settings/Settings Targets/Location Reminders/LocationRemindersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationRemindersTableViewCell.swift
+//  ItemTracker
+//
+//  Created by Bree Chelle on 2019-07-26.
+//  Copyright © 2019 Brock Chelle. All rights reserved.
+//
+
+import UIKit
+
+class LocationRemindersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
